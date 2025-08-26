d9915298ad4acfaf8c7c0b93fdbc8c838f89ca76
Jason Yandell <jasonyandell@gmail.com>
2025-08-25 21:45:51 -0500

    github pages

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import { svelte } from '@sveltejs/vite-plugin-svelte'
 
 export default defineConfig({
   plugins: [svelte()],
+  base: process.env.NODE_ENV === 'production' ? '/mk5-main/' : '/',
   server: {
     port: parseInt(process.env.PORT || '60101'),
     open: true
